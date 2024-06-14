c60499c29c740aa2c8712f8375551fec5f222751
djl1626 <mrclutchen@gmail.com>
2024-06-14 16:11:09 -0400

    presentation histogram

--- Python/presentation-histogram.py
+++ Python/presentation-histogram.py
@@ -5,12 +5,10 @@ import pandas as pd
 data = pd.read_excel(
     "/Users/djl47/Documents/Graduate School/uva-msds-orientation-technical/Python/MSDS-Orientation-Computer-Survey.xlsx")
 
-# fig, ax = plt.subplots(1, 1)
-plt.hist(data[data['CPU Number of Cores (int)'] > 0]
-         [['CPU Number of Cores (int)']], bins=10, color="pink", edgecolor="black")
+plt.hist(data[data['CPU Number of Cores (int)'] > 0][[
+         'CPU Number of Cores (int)']], bins=10, color="pink", edgecolor="black")
 plt.title("Distribution of the Number of CPU Cores")
 plt.xlabel("Number of Cores")
 plt.ylabel("Frequency")
-plt.show()
 plt.savefig(
     "/Users/djl47/Documents/Graduate School/uva-msds-orientation-technical/Python/presentation-fig.png")
